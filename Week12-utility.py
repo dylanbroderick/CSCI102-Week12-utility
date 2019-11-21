c53ece2d7b5bb8e92fc1d90b242b4cc7f92faf1b
dylanbroderick <dylanbroderick@mymail.mines.edu>
2019-11-21 12:36:08 -0700

    progress2 FindWordCount

--- Week12-utility.py
+++ Week12-utility.py
@@ -19,9 +19,15 @@ def UpdateString(string1,string2,index):
     up_string = separator.join(S1LIST)
     return up_string
 
-def FindWordCount(list1,string):
-    space = " "
+def FindWordCount(list1,a):
+    space = ""
     string1 = space.join(list1)
     i = 0
+    count = 0
     for s in string1:
-        
+        if a in string1[5:]:
+            count += 1
+            i += string1.find(a)
+            #print(i)
+    print(count)
+
